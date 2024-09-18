9aef7dfc9206d94b09c77457e377caf0be1f837c
Arian <arian.kulmer@web.de>
2024-09-18 18:28:43 +0200

    Pong: Allow setsockopt syscall for qcom c2audio
    ```strace -f vendor/bin/hw/vendor.qti.media.c2@1.0-service```
    
    ```
    [...]
    [pid  5194] socket(AF_UNIX, SOCK_SEQPACKET|SOCK_CLOEXEC, 0) = 13
    [pid  5194] setsockopt(13, SOL_SOCKET, SO_SNDTIMEO, "\5\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0", 16) = 13
    [pid  5194] --- SIGSYS {si_signo=SIGSYS, si_code=SYS_SECCOMP, si_call_addr=0x7a905a365c, si_syscall=__NR_setsockopt, si_arch=AUDIT_ARCH_AARCH64} ---
    ```
    
    Change-Id: Ifa0420883cbf663f8c7576798b1f35031f660f54
    Signed-off-by: N0m4n904 <noah.anleitner@halogenos.org>

--- extract-files.sh
+++ extract-files.sh
@@ -73,7 +73,11 @@ function blob_fixup() {
             ;;
         vendor/etc/media_codecs_cape.xml|vendor/etc/media_codecs_cape_vendor.xml)
             sed -Ei "/media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio)/d" "${2}"
-	    ;;
+	        ;;
+        vendor/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy)
+            [ "$2" = "" ] && return 0
+            grep -q "setsockopt: 1" "${2}" || echo "setsockopt: 1" >> "${2}"
+            ;;
         vendor/lib64/vendor.libdpmframework.so)
             ${PATCHELF} --add-needed "libhidlbase_shim.so" "${2}"
             ;;
