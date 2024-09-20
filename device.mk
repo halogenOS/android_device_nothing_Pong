7d6fea05a34527b381c70ec69034a3c5c3ce3b8e
N0m4n904 <noah.anleitner@halogenos.org>
2024-09-20 22:57:12 +0200

    Revert "Pong: Checkout Dolby from hardware_xiaomi"
    This reverts commit 918c1bbb9321abfc216694397127bec2fff0a931.

--- device.mk
+++ device.mk
@@ -195,9 +195,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
 # Dolby
-PRODUCT_PACKAGES += \
-    PartsDolby
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
