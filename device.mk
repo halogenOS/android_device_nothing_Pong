9b3888d932194e5e0b1099da90e57aeeb1cf60ca
N0m4n904 <noah.anleitner@halogenos.org>
2024-09-20 22:58:57 +0200

    Revert "Pong: Import media_codecs_dolby_audio to device tree"
    This reverts commit 5cdf0a6817ad8121a024d2e54ed174405e93bcc2.

--- device.mk
+++ device.mk
@@ -321,8 +321,7 @@ PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/common/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/media_codecs_cape_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_cape_vendor.xml \
-    $(LOCAL_PATH)/audio/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
+    $(LOCAL_PATH)/audio/media_codecs_cape_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_cape_vendor.xml
 
 PRODUCT_PACKAGES += \
     libavservices_minijail \
