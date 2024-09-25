7f8e0214f965a888b7cf27ffc95828c61e454ff0
N0m4n904 <noah.anleitner@halogenos.org>
2024-09-25 19:30:51 +0200

    Pong: Allow gettid syscall for wfdhdcphalservice

--- extract-files.sh
+++ extract-files.sh
@@ -73,6 +73,10 @@ function blob_fixup() {
         vendor/lib64/vendor.libdpmframework.so)
             ${PATCHELF} --add-needed "libhidlbase_shim.so" "${2}"
             ;;
+        /vendor/etc/seccomp_policy/wfdhdcphalservice.policy)
+            [ "$2" = "" ] && return 0
+            grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
+            ;;
     esac
 }
 
