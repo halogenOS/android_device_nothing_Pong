d56ed713a6ce426657f7c7e63e77672caa0756f9
N0m4n904 <noah.anleitner@halogenos.org>
2024-09-25 20:57:39 +0200

    remove some stuff

--- extract-files.sh
+++ extract-files.sh
@@ -68,21 +68,18 @@ function blob_fixup() {
             "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
             ;;
         vendor/etc/seccomp_policy/atfwd@2.0.policy)
-            [ "$2" = "" ] && return 0
             grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
             ;;
         vendor/etc/media_codecs_cape.xml|vendor/etc/media_codecs_cape_vendor.xml)
             sed -Ei "/media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio)/d" "${2}"
 	        ;;
         vendor/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy)
-            [ "$2" = "" ] && return 0
             grep -q "setsockopt: 1" "${2}" || echo "setsockopt: 1" >> "${2}"
             ;;
         vendor/lib64/vendor.libdpmframework.so)
             ${PATCHELF} --add-needed "libhidlbase_shim.so" "${2}"
             ;;
         /vendor/etc/seccomp_policy/wfdhdcphalservice.policy)
-            [ "$2" = "" ] && return 0
             grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
             ;;
     esac
