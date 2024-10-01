7fe60171f20384cd5b7537051a111fbf2105870d
Simão Gomes Viana <devel@superboring.dev>
2024-10-02 00:24:44 +0200

    Sync fingerprint AIDL implementation with AOSP
    This is basically a rewrite, adhering to what you find in
    hardware/interfaces/fingerprint/aidl/default
    
    So far it behaves exactly the same as the previous implementation did.

--- fingerprint/include/CancellationSignal.h
+++ fingerprint/include/CancellationSignal.h
@@ -1,34 +1,21 @@
 /*
  * Copyright (C) 2024 The LineageOS Project
+ * Copyright (C) 2024 The halogenOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-
 #pragma once
-
 #include <aidl/android/hardware/biometrics/common/BnCancellationSignal.h>
-
 #include "Session.h"
 
 using ::aidl::android::hardware::biometrics::common::BnCancellationSignal;
 
-namespace aidl {
-namespace android {
-namespace hardware {
-namespace biometrics {
-namespace fingerprint {
-
+namespace aidl::android::hardware::biometrics::fingerprint {
 class CancellationSignal : public BnCancellationSignal {
 public:
     CancellationSignal(Session* session);
     ndk::ScopedAStatus cancel() override;
-
 private:
     Session* mSession;
 };
-
-} // namespace fingerprint
-} // namespace biometrics
-} // namespace hardware
-} // namespace android
-} // namespace aidl
+} // namespace aidl::android::hardware::biometrics::fingerprint
