66d1825a8ab24a0f642469bb8e9589b7dc10a69e
N0m4n904 <noah.anleitner@halogenos.org>
2024-10-05 19:28:51 +0200

    Pong: Remove overlay-lineage We are using RRO overlays

--- device.mk
+++ device.mk
@@ -367,9 +367,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 # QSSI overlays
 PRODUCT_PACKAGES += \
     FrameworksResCommon \
