5ed5d99286c6442ec4ab719ef622ee35a8cb1649
Simão Gomes Viana <devel@superboring.dev>
2024-10-05 13:54:31 +0000

    Remove nothing bloat
    we don't even use this
    
    Signed-off-by: N0m4n904 <noah.anleitner@halogenos.org>

--- device.mk
+++ device.mk
@@ -354,13 +354,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
-# Nothing-fwk
-PRODUCT_PACKAGES += \
-    nothing-fwk
-
-PRODUCT_BOOT_JARS += \
-    nothing-fwk
-
 # Overlays
 PRODUCT_PACKAGES += \
     PongApertureRes \
