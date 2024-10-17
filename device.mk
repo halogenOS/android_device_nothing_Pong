1d8deaeb3c7a4fd7b1831fed6b41aadbc4b1b178
N0m4n904 <noah.anleitner@halogenos.org>
2024-10-17 19:47:47 +0200

    Revert "Pong: Use QCOM USB gadget HAL"
    This reverts commit 2f2146913657d8146cd8648ab9f61452232ca337.

--- device.mk
+++ device.mk
@@ -510,11 +510,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti \
-    android.hardware.usb.gadget@1.2-service-qti
-
-PRODUCT_PACKAGES += \
-    usb_compositions.conf
+    android.hardware.usb@1.3-service-qti
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
