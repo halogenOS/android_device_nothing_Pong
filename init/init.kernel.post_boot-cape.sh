1d3f2db65b945b18befacde270cc555ec42c8233
Ghosuto <clash.raja10@gmail.com>
2025-02-02 15:32:13 +0000

    Pong: init: Separate restricted cpuset definition from system-background
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: N0m4n904 <noah.anleitner@halogenos.org>

--- init/init.kernel.post_boot-cape.sh
+++ init/init.kernel.post_boot-cape.sh
@@ -57,10 +57,8 @@ echo 325 > /proc/sys/walt/walt_low_latency_task_threshold
 # cpuset parameters
 echo 0-1 > /dev/cpuset/background/cpus
 echo 0-2 > /dev/cpuset/system-background/cpus
+echo 0-2 > /dev/cpuset/restricted/cpus
 echo 0-6 > /dev/cpuset/foreground/cpus
-
-# Set restricted cpuset to the same CPUs as system-background
-cat /dev/cpuset/system-background/cpus > /dev/cpuset/restricted/cpus
 
 # Turn off scheduler boost at the end
 echo 0 > /proc/sys/walt/sched_boost
