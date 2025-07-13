2386407acc2a9bf39d9b15f73d765f02149d30cd
Noah Anleitner <noah.anleitner@nonetwor.cc>
2025-07-13 07:52:40 +0200

    remove leftover nt-fwk

--- device.mk
+++ device.mk
@@ -238,17 +238,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
-# NT-fwk
-PRODUCT_PACKAGES += \
-    nt-fwk.pong
-
-PRODUCT_BOOT_JARS += \
-    nt-fwk.pong
-
-# NT-glyph
-PRODUCT_PACKAGES += \
-    PongGlyph
-
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
