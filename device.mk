29168eebc39639aed45a3b6ce6d103756942c603
Noah Anleitner <noah.anleitner@nonetwor.cc>
2025-07-13 07:56:31 +0200

    CUSTOM_PRODUCT_DIR

--- device.mk
+++ device.mk
@@ -406,7 +406,7 @@ PRODUCT_COPY_FILES += \
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(LOCAL_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    $(CUSTOM_PRODUCT_DIR)/config/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += device/nothing/Pong/framework_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest_cape.xml
