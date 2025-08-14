b0b166ab67feaf26142dd408f4012b2c14dd2993
Noah Anleitner <noah.anleitner@nonetwor.cc>
2025-08-14 17:28:45 +0200

    Pong: fingerprint: Switch to AIDL fingerprint service
    Use AIDL we had on the XOS-15.2 branch: https://git.halogenos.org/halogenOS/android_device_nothing_Pong/-/commits/XOS-15.2/fingerprint

--- device.mk
+++ device.mk
@@ -135,12 +135,12 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.nt.pong
+    android.hardware.biometrics.fingerprint-service.nothing
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
-$(call soong_config_set,surfaceflinger,udfps_lib,//$(LOCAL_PATH):libudfps_extension.nt.pong)
+$(call soong_config_set,surfaceflinger,udfps_lib,//$(LOCAL_PATH):libudfps_extension.nothing)
 
 # Fastboot
 PRODUCT_PACKAGES += \
