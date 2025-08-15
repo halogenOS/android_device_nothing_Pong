70c4574d2d4be396e1fe94df5780d007c9105aa0
Noah Anleitner <noah.anleitner@nonetwor.cc>
2025-08-15 16:45:18 +0200

    Pong: build Aperture and Dialer overlay

--- device.mk
+++ device.mk
@@ -239,6 +239,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
 # Overlays
+PRODUCT_PACKAGES += \
+    PongApertureRes \
+    PongDialerRes
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Qssi overlays
