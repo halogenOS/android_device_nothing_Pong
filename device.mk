af70930a70e057e5346bd74405793ef7dc32238a
Noah Anleitner <noah.anleitner@nonetwor.cc>
2025-08-15 16:43:20 +0200

    Pong: remove overlay-lineage leftover

--- device.mk
+++ device.mk
@@ -241,9 +241,6 @@ PRODUCT_COPY_FILES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 # Qssi overlays
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
