8169e1a12fd8b269a73040838c537614215aa563
Noah Anleitner <noah.anleitner@nonetwor.cc>
2026-05-09 19:43:10 +0200

    Pong: Update from NOS B4.1-260414-1749
    Change-Id: If7f81110fb6e71d8f6bbb2b6219ae44ce0413127

--- aosp_Pong.mk
+++ aosp_Pong.mk
@@ -25,8 +25,8 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_GMS_CLIENTID_BASE := android-nothing
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi-user 16 BQ2A.250721.001-BP2A.250605.031.A3 2512261110 release-keys" \
-    BuildFingerprint=Nothing/Pong/Pong:12/SKQ1.250415.001/2512261110:user/release-keys \
+    BuildDesc="qssi-user 16 BQ2A.250721.001-BP2A.250605.031.A3 2604141749 release-keys" \
+    BuildFingerprint=Nothing/Pong/Pong:16/BQ2A.250721.001-BP2A.250605.031.A3/2604141749:user/release-keys \
     DeviceName=Pong \
     DeviceProduct=Pong \
     SystemDevice=Pong \
