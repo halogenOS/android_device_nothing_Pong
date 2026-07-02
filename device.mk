2bd24fc913eb238a4b321ae79938a61332c685a7
Noah Anleitner <noah.anleitner@nonetwor.cc>
2026-07-02 18:17:37 +0200

    fixup! Pong: Enable VRR mode by default for LTPO

--- device.mk
+++ device.mk
@@ -273,9 +273,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 PRODUCT_PACKAGES += \
     custom.hardware.display.ltpo-service.pong
 
-PRODUCT_VENDOR_PROPERTIES += \
-    persist.sys.sfm.mode=vrr
-
 # Glyph
 PRODUCT_PACKAGES += \
     ParanoidGlyphPhone2 \
